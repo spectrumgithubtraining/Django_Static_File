25204ad7361c4efc06b544d920fe5d1ccf880cca
Spectrum Training <150805887+spectrumgithubtraining@users.noreply.github.com>
2023-11-16 15:52:32 +0530

    Add files via upload

---
+++ testproject/testproject/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin  
+from django.urls import path  
+from myapp import views  
+
+urlpatterns = [  
+    path('admin/', admin.site.urls),  
+    path('index/', views.index),  
+    ]     
